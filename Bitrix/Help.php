eadfb85b694284a7fc11719a8a06fa1eccc70e20
Artem Medvedev <medvedev@dialweb.ru>
2021-03-25 20:39:07 +0300

    Remain file

---
+++ Bitrix/Help.php
@@ -0,0 +1,23 @@
+<!-- Вывод масива, только для админа -->
+<?
+global $USER;
+if($USER -> IsAdmin()){
+    echo('<pre>');
+    print_r($yourArray);
+}
+?>
+<!-- Вывод масива, только для админа -->
+
+<!-- Получение пользовательских свойств раздела -->
+<?$fSections = CIBlockSection::GetList(
+    false,
+    Array("IBLOCK_ID" => /*Тут id инфоблока*/ , "ID" => /*Тут id раздела*/ , "ACTIVE"=>"Y", "GLOBAL_ACTIVE"=>"Y", "SECTION_ACTIVE" => "Y"),
+    false,
+    Array("Здесь название св-ва"),
+    false
+);    
+    $flSections = $fSections->Fetch();
+    $links =  $flSections["Здесь название св-ва"];
+    echo $links;
+?>
+<!-- Получение пользовательских свойств раздела -->
